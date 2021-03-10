0185a660898b268968e84df73d97e2d404713dd7
DoctorrHavoc <35706573+DoctorrHavoc@users.noreply.github.com>
2021-03-10 12:14:15 +0200

    Add files via upload

---
+++ LaserDioide.ino
@@ -0,0 +1,17 @@
+int Laser = 6;
+int Detector = 7;
+
+void setup()
+{ 
+  Serial.begin (9600);
+  pinMode(Laser, OUTPUT);
+  pinMode(Detector, INPUT);
+}
+
+void loop()
+{
+  digitalWrite(Laser, HIGH);
+  boolean val = digitalRead(Detector);
+  Serial.println(val);
+} 
+
